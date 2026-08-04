189d81a408136ab123fac26610d8e614ad301b8d
Your Name <rafaelcaruana@outlook.com>
2026-08-04 12:25:34 +0100

    Improved stock API

--- app.py
+++ app.py
@@ -5,45 +5,89 @@ import math
 
 app = Flask(__name__)
 
-# Popular Roblox game IDs
-# We can expand this to 100 later
+
+# Roblox Universe IDs
 GAME_IDS = {
     "Grow a Garden": 7436755782,
-    "Brookhaven 🏡RP": 4924922222,
-    "Blox Fruits": 2753915549,
     "Adopt Me": 383310974,
-    "Murder Mystery 2": 66654135
+    "Murder Mystery 2": 66654135,
+    "Blox Fruits": 2753915549,
+    "Brookhaven": 4924922222,
+    "Pet Simulator 99": 8737899170,
+    "Tower of Hell": 1962086868,
+    "Doors": 6516141723,
+    "Jailbreak": 606849621,
+    "Arsenal": 286090429
 }
 
 
-def get_game_data():
-    universe_ids = ",".join(str(x) for x in GAME_IDS.values())
+# Stores previous player counts
+previous_players = {}
 
-    url = f"https://games.roblox.com/v1/games?universeIds={universe_ids}"
+
+def calculate_price(players):
+    """
+    Converts player count into stock price
+    """
+    return round(math.log(players + 1) * 100, 2)
+
+
+def calculate_change(name, players):
+    """
+    Calculates percentage player change
+    """
+    old_players = previous_players.get(name, players)
+
+    if old_players == 0:
+        change = 0
+    else:
+        change = ((players - old_players) / old_players) * 100
+
+    previous_players[name] = players
+
+    return round(change, 2)
+
+
+
+def get_game_data():
+
+    universe_ids = ",".join(
+        str(id) for id in GAME_IDS.values()
+    )
+
+    url = (
+        "https://games.roblox.com/v1/games"
+        f"?universeIds={universe_ids}"
+    )
 
     response = requests.get(url)
 
     if response.status_code != 200:
         return []
 
+
     data = response.json()
 
     stocks = []
+
 
     for game in data["data"]:
 
         name = game["name"]
         players = game["playing"]
 
-        # Convert players into stock price
-        price = round(math.log(players + 1) * 100, 2)
+        price = calculate_price(players)
+        change = calculate_change(name, players)
+
 
         stocks.append({
             "name": name,
             "players": players,
             "price": price,
+            "change": change,
             "visits": game["visits"]
         })
+
 
     return stocks
 
